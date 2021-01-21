c889521e796b2af6a1407ab9ae254abe8bf87ca8
Patrik Badanjak <pbadanj@racunarstvo.hr>
2021-01-21 10:16:47 +0100

    Redirect to Login

--- app/src/main/java/hr/algebra/pbadanjak/flashcards/MainActivity.kt
+++ app/src/main/java/hr/algebra/pbadanjak/flashcards/MainActivity.kt
@@ -1,11 +1,18 @@
 package hr.algebra.pbadanjak.flashcards
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hr.algebra.pbadanjak.flashcards.framework.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) startLoginActivity()
     }
+
+    private fun startLoginActivity() =
+        startActivity<LoginActivity>()
 }
